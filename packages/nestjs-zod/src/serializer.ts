5aeea2522091a180a343fd14050d68c4c507ea33
Daniel Peter Volquardsen <daniel@volquardsen.dev>
2025-08-14 07:08:59 +0200

    feat: Enhance ZodSerializerDto to support async parsing

--- packages/nestjs-zod/src/serializer.ts
+++ packages/nestjs-zod/src/serializer.ts
@@ -7,12 +7,12 @@ import {
   SetMetadata,
   StreamableFile,
 } from '@nestjs/common'
-import { map, Observable } from 'rxjs'
-import { ZodDto } from './dto'
-import { validate } from './validate'
+import { Observable, from, of, throwError } from 'rxjs'
+import { catchError, mergeMap } from 'rxjs/operators'
+import { assert } from './assert'
+import { ZodDto, isZodDto } from './dto'
 import { createZodSerializationException } from './exception'
 import { UnknownSchema } from './types'
-import { assert } from './assert'
 
 // NOTE (external)
 // We need to deduplicate them here due to the circular dependency
@@ -21,13 +21,22 @@ const REFLECTOR = 'Reflector'
 
 export const ZodSerializerDtoOptions = 'ZOD_SERIALIZER_DTO_OPTIONS' as const
 
-export function ZodSerializerDto(dto: ZodDto<UnknownSchema> | UnknownSchema | [ZodDto<UnknownSchema>] | [UnknownSchema]) {
+export function ZodSerializerDto(
+  dto:
+    | ZodDto<UnknownSchema>
+    | UnknownSchema
+    | [ZodDto<UnknownSchema>]
+    | [UnknownSchema]
+) {
   if (Array.isArray(dto)) {
     const schema = 'schema' in dto[0] ? dto[0].schema : dto[0]
-    assert('array' in schema && typeof schema.array === 'function', 'ZodSerializerDto was used with array syntax (e.g. `ZodSerializerDto([MyDto])`) but the DTO schema does not have an array method')
+    assert(
+      'array' in schema && typeof schema.array === 'function',
+      'ZodSerializerDto was used with array syntax (e.g. `ZodSerializerDto([MyDto])`) but the DTO schema does not have an array method'
+    )
   }
-  
-  return SetMetadata(ZodSerializerDtoOptions, dto);
+
+  return SetMetadata(ZodSerializerDtoOptions, dto)
 }
 
 @Injectable()
@@ -38,25 +47,56 @@ export class ZodSerializerInterceptor implements NestInterceptor {
     const responseSchema = this.getContextResponseSchema(context)
 
     return next.handle().pipe(
-      map((res: object | object[]) => {
-        if (!responseSchema) return res
-        if (typeof res !== 'object' || res instanceof StreamableFile) return res
+      mergeMap((res: object | object[]) => {
+        if (!responseSchema) return of(res)
+        if (typeof res !== 'object' || res instanceof StreamableFile)
+          return of(res)
 
+        const resolveSchema = (
+          schemaOrDto: UnknownSchema | ZodDto<UnknownSchema>
+        ): UnknownSchema =>
+          isZodDto(schemaOrDto) ? schemaOrDto.schema : schemaOrDto
+
+        // Array syntax: @ZodSerializerDto([Dto])
         if (Array.isArray(responseSchema)) {
-          const schema = 'schema' in responseSchema[0] ? responseSchema[0].schema : responseSchema[0]
-          assert('array' in schema && typeof schema.array === 'function', 'ZodSerializerDto was used with array syntax (e.g. `ZodSerializerDto([MyDto])`) but the DTO schema does not have an array method')
-
-          return validate(res, schema.array(), createZodSerializationException)
+          const baseSchema = resolveSchema(responseSchema[0])
+          assert(
+            typeof baseSchema.array === 'function',
+            'ZodSerializerDto was used with array syntax (e.g. `ZodSerializerDto([MyDto])`) but the DTO schema does not have an array method'
+          )
+          const arraySchema = baseSchema.array()
+          const promise = arraySchema.parseAsync
+            ? arraySchema.parseAsync(res)
+            : Promise.resolve(arraySchema.parse(res))
+          return from(promise).pipe(
+            catchError((error) =>
+              throwError(() => createZodSerializationException(error))
+            )
+          )
         }
 
-        return validate(res, responseSchema, createZodSerializationException)
+        // Single schema
+        const schema = resolveSchema(responseSchema)
+        const promise = schema.parseAsync
+          ? schema.parseAsync(res)
+          : Promise.resolve(schema.parse(res))
+        return from(promise).pipe(
+          catchError((error) =>
+            throwError(() => createZodSerializationException(error))
+          )
+        )
       })
     )
   }
 
   protected getContextResponseSchema(
     context: ExecutionContext
-  ): ZodDto<UnknownSchema> | UnknownSchema | [ZodDto<UnknownSchema>] | [UnknownSchema] | undefined {
+  ):
+    | ZodDto<UnknownSchema>
+    | UnknownSchema
+    | [ZodDto<UnknownSchema>]
+    | [UnknownSchema]
+    | undefined {
     return this.reflector.getAllAndOverride(ZodSerializerDtoOptions, [
       context.getHandler(),
       context.getClass(),
--- packages/nestjs-zod/src/types.ts
+++ packages/nestjs-zod/src/types.ts
@@ -1,5 +1,6 @@
 export interface UnknownSchema {
     parse(input: unknown): unknown;
+    parseAsync?: (input: unknown) => Promise<unknown>;
     array?: () => UnknownSchema;
 }
 
